fe282d8e509eead003ddcef976383855b67e0ff5
shahzodbeeck <shahzodomonboyev1.com>
2024-12-21 23:58:40 +0500

    commit message

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-a+rpbjo-m1tft+0vsrd*a9fdypo%rrh59&+-o(4-3+vao!#ej%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
