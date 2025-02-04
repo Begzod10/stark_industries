7bef88a9d2bccec9757c9c7eb588a9a6e4b82152
Begzod10 <rimefara22@gmail.com>
2025-02-04 22:24:26 +0500

    commit

--- users/user_analysis/api/get/list.py
+++ users/user_analysis/api/get/list.py
@@ -1,13 +1,18 @@
 from rest_framework.generics import ListAPIView
-
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from users.models.analysis import UserAnalysis
 from users.user_analysis.serializers.get.retrieve import UserAnalysisGetSerializer
+import pprint
 
 
 class UsersAnalysisList(ListAPIView):
     queryset = UserAnalysis.objects.all()
     serializer_class = UserAnalysisGetSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user']
 
     def list(self, request, *args, **kwargs):
-        self.queryset = UserAnalysis.objects.filter(user=request['user'])
-        return self.get_queryset()
+        queryset = self.filter_queryset(self.get_queryset())  # Apply filters properly
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)  # Return as Response
