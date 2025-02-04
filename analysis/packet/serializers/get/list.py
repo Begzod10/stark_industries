ad9ed9a36e05b5291dbb011bc36a8674258b99e0
thesobirov <shahzodsobirjonov9@gmail.com>
2025-02-04 13:17:03 +0500

    asfdasfd

--- analysis/packet/serializers/get/list.py
+++ analysis/packet/serializers/get/list.py
@@ -6,7 +6,11 @@ from analysis.analysis.serializers.get.get import AnalysisGetSerializer
 
 class PacketListSerializer(serializers.ModelSerializer):
     analysis = AnalysisGetSerializer(many=True, read_only=True, source='analysis_set')
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Packet
-        fields = ['id', 'name', 'analysis']
+        fields = ['id', 'name', 'analysis', 'total_price']
+
+    def get_total_price(self, obj):
+        return sum(analysis.price for analysis in obj.analysis_set.all())
