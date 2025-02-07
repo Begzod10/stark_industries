32c7290ebd83e0bd9e391bd4cc38c91becacec04
thesobirov <shahzodsobirjonov9@gmail.com>
2025-02-07 20:09:52 +0500

    sdfsd

--- users/users/api/crud/create.py
+++ users/users/api/crud/create.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from users.models.user import User
 from users.users.serializers.crud.crud import UserCrudSerializer
@@ -7,3 +10,15 @@ from users.users.serializers.crud.crud import UserCrudSerializer
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCrudSerializer
+
+
+class UseraddRequest(RetrieveUpdateAPIView):
+    serializer_class = UserCrudSerializer
+    queryset = User.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- users/users/serializers/crud/crud.py
+++ users/users/serializers/crud/crud.py
@@ -44,37 +44,94 @@ class UserCrudSerializer(serializers.ModelSerializer):
                 setattr(instance, attr, value)
             instance.save()
 
-        user_request = UserRequest.objects.filter(patient=instance).first()
+        user_request, created = UserRequest.objects.get_or_create(patient=instance)
 
         if doctor and from_date and to_date and date:
-            if user_request:
-                user_request.doctor = doctor
-                user_request.from_date = from_date
-                user_request.to_date = to_date
-                user_request.date = date
-                user_request.save()
-            else:
-                user_request = UserRequest.objects.create(
-                    doctor=doctor,
-                    patient=instance,
-                    from_date=from_date,
-                    to_date=to_date,
-                    date=date
-                )
+            user_request.doctor = doctor
+            user_request.from_date = from_date
+            user_request.to_date = to_date
+            user_request.date = date
+            user_request.save()
 
-        if user_request and analysis:
-            UserAnalysis.objects.filter(user=instance, request=user_request).delete()
+        if analysis:
             for analysis_item in analysis:
-                UserAnalysis.objects.create(
+                UserAnalysis.objects.get_or_create(
                     user=instance,
                     analysis=analysis_item,
                     request=user_request,
-                    status=False,
-                    expected_result=None,
-                    result=None
+                    defaults={'status': False, 'expected_result': None, 'result': None}
                 )
 
         return instance
+
+# class UserCrudSerializer(serializers.ModelSerializer):
+#     branch = serializers.PrimaryKeyRelatedField(queryset=Branch.objects.all())
+#     doctor_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False, write_only=True)
+#     from_date = serializers.TimeField(required=False, write_only=True)
+#     to_date = serializers.TimeField(required=False, write_only=True)
+#     date = serializers.DateField(required=False, write_only=True)
+#     analysis = serializers.PrimaryKeyRelatedField(queryset=Analysis.objects.all(), required=False, write_only=True,
+#                                                   many=True)
+#     email = serializers.EmailField(required=False, allow_null=True, allow_blank=True)
+#
+#     class Meta:
+#         model = User
+#         fields = [
+#             'name', 'surname', 'birth_date', 'phone_number', 'address', 'password', 'sex', 'branch', 'username',
+#             'email', 'passport_series', 'passport_number', 'doctor_id', 'from_date', 'to_date', 'date', 'analysis'
+#         ]
+#
+#     def create(self, validated_data):
+#         return self._save_user(None, validated_data)
+#
+#     def update(self, instance, validated_data):
+#         return self._save_user(instance, validated_data)
+#
+#     def _save_user(self, instance, validated_data):
+#         doctor = validated_data.pop('doctor_id', None)
+#         from_date = validated_data.pop('from_date', None)
+#         to_date = validated_data.pop('to_date', None)
+#         date = validated_data.pop('date', None)
+#         analysis = validated_data.pop('analysis', None)
+#
+#         if instance is None:
+#             instance = User.objects.create(**validated_data)
+#         else:
+#             for attr, value in validated_data.items():
+#                 setattr(instance, attr, value)
+#             instance.save()
+#
+#         user_request = UserRequest.objects.filter(patient=instance).first()
+#
+#         if doctor and from_date and to_date and date:
+#             if user_request:
+#                 user_request.doctor = doctor
+#                 user_request.from_date = from_date
+#                 user_request.to_date = to_date
+#                 user_request.date = date
+#                 user_request.save()
+#             else:
+#                 user_request = UserRequest.objects.create(
+#                     doctor=doctor,
+#                     patient=instance,
+#                     from_date=from_date,
+#                     to_date=to_date,
+#                     date=date
+#                 )
+#
+#         if user_request and analysis:
+#             UserAnalysis.objects.filter(user=instance, request=user_request).delete()
+#             for analysis_item in analysis:
+#                 UserAnalysis.objects.create(
+#                     user=instance,
+#                     analysis=analysis_item,
+#                     request=user_request,
+#                     status=False,
+#                     expected_result=None,
+#                     result=None
+#                 )
+#
+#         return instance
 
 # class UserCrudSerializer(serializers.ModelSerializer):
 #     branch = serializers.PrimaryKeyRelatedField(queryset=Branch.objects.all())
