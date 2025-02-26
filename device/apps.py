8eb06e00817aa772d2248ead072abf20803754cd
shahzodbeeck <shahzodomonboyev1.com>
2025-02-26 10:57:08 +0500

    commit message

--- device/apps.py
+++ device/apps.py
@@ -1,6 +1,15 @@
+import threading
+
 from django.apps import AppConfig
 
 
 class DeviceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'device'
+
+    def ready(self):
+        from device.server.snibe import run_servers
+        if not hasattr(self, 'server_thread'):
+            self.server_thread = threading.Thread(target=run_servers, daemon=True)
+            self.server_thread.start()
+            print("ASTM servers started in background")
